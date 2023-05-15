03d088088a478cb80ce02b025378801be2896770
JanCarlo03 <al221910462@gmail.com>
2023-05-15 11:58:03 -0600

    Diseño de tablas y boton

--- resources/js/Pages/Usuarios/Index.vue
+++ resources/js/Pages/Usuarios/Index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Barra from "@/components/Barra.vue";
-import { Head } from '@inertiajs/vue3'
+import { Head } from '@inertiajs/vue3';
 
 defineProps({ usuarios: Object })
 </script>
@@ -10,8 +10,15 @@ defineProps({ usuarios: Object })
         <div class="mx-auto max-w-2xl text-center">
           <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Administracion de usuarios</h2>
         </div>
-        <div class="mx-auto max-w-2xl text-center sm:py-10 lg:px-8">
+        <div class="mx-auto max-w-2xl text-right sm:py-10 lg:px-8">
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <div class="relative h-15 w-29.5">
+                    <button class="relative top-0 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                        <span class="relative px-2 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                            <font-awesome-icon icon="fas fa-user-plus" />
+                        </span>
+                    </button>
+                </div>
                 <table class="w-full text-sm text-left text-black-500">
                     <thead class="text-xs text-white-1000 uppercase bg-white-50 ">
                         <tr>
@@ -37,6 +44,8 @@ defineProps({ usuarios: Object })
                             <td class="px-6 py-4">{{ usuario.id }}</td>
                             <td class="px-6 py-4">{{ usuario.name }}</td>
                             <td class="px-6 py-4">{{ usuario.email }}</td>
+                            <td class="px-10 py-4"><a href=""><font-awesome-icon icon="far fa-pen-to-square" /></a></td>
+                            <td class="px-10 py-4"><a href=""><font-awesome-icon icon="far fa-trash-can" /></a></td>
                         </tr>
                     </tbody>
                 </table>
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,16 +5,26 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { dom } from "@fortawesome/fontawesome-svg-core";
+
+dom.watch();
+library.add(far,fas,fab);
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
-
 createInertiaApp({
+    
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .component('font-awesome-icon', FontAwesomeIcon)
             .mount(el);
     },
     progress: {
